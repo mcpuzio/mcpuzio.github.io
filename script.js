210a1c120604db632e105357a8e2af88a542a080
Mike Puzio <mcpuzio@gmail.com>
2016-03-03 11:41:34 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+		// $("body").click(
+		// function(){
+		// 	$("#wrapper1").slideUp();
+
+		// 	$("h1").fadeIn(2000);
+		// 		});
+		// $("p").click(function(){
+  //       $(this).hide();
+  		$("body").hover(
+		function(){
+			$("#h4").hide({
+    	});	});
+
+    	$("body").mousedown(
+		function(){
+			$("#wrapper1").animate({
+        left: '550px',
+        height: '150px',
+        width: '150px',
+        speed: 5000,
+            	});	});
+    });
+
+$(document).ready(function() {
+	$("body").mouseenter(
+		function(){
+			$("#ptlist"&"ptlist2").fadeIn({
+        300,
+    	});	}); });
