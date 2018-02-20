dbb75542ec8ab95a872e104847c1da2afcca1bf9
Mike Puzio <mcpuzio@gmail.com>
2018-02-20 02:07:53 +0100

    2018 update
    This is screwy; trying to update the portfolio website

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function myMap() {
 var mapProp = {
     center: new google.maps.LatLng(50.061728, 19.937405),
@@ -36,3 +37,18 @@ function toggleFunction() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+=======
+$(document).ready(function() {
+	$('body').mousemove(function() {
+		$('h1').fadeIn(2300);
+	});
+
+});
+
+// $(document).ready(function() {
+// 	$("body").mouseenter(
+// 		function(){
+// 			$("#ptlist"&"ptlist2").fadeIn({
+//         300,
+//     	});	}); });
+>>>>>>> Stashed changes
